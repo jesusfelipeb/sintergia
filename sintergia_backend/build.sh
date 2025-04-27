f20b4a6ea662ea01ac2af92e33f2c7f61310ff56
felipe <jesusbecerra91@gmail.com>
2025-04-27 17:33:51 -0300

    Añadir script de construcción para Render

---
+++ sintergia_backend/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Instalar dependencias
+pip install -r requirements.txt
+
+# Recolectar archivos estáticos
+python manage.py collectstatic --no-input
+
+# Aplicar migraciones
+python manage.py migrate
